bab4fc7b3dec1e8abf14275a8c4ed657f7848652
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-14 23:12:35 +0300

    a

--- user-data.sh
+++ user-data.sh
@@ -4,4 +4,4 @@ sudo yum install git -y && \
 git clone https://github.com/frnitzsche/docker-compose-confluence.git && \
 cd docker-compose-confluence && \
 chmod a+x customization-script.sh && \
-/bin/bash customization-script.sh
+/bin/bash customization-script.sh > /var/log/custamization-script.log 2>&1
